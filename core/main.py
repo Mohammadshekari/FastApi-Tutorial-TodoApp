9d785260cec06340b5051ec35c76a347849f8d0e
Mamad <m.shekari79b5@gmail.com>
2025-03-06 21:46:44 +0330

    Config env and database

--- core/main.py
+++ core/main.py
@@ -1,3 +1,13 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("App Start...")
+    yield
+    print("App Finish...")
+
+
+app = FastAPI(lifespan=lifespan)
