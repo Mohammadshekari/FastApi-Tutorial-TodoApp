4cd21c2af7aea28f0893a92c29f1de1dc3dbd27e
Mamad <m.shekari79b5@gmail.com>
2025-03-07 21:24:11 +0330

    Add Password hash system

--- core/users/models.py
+++ core/users/models.py
@@ -2,8 +2,21 @@ import datetime
 
 from sqlalchemy import Column, String, Text, Integer, Boolean, func, DateTime
 from sqlalchemy.orm import relationship
+from passlib.context import CryptContext
 
 from core.database import Base
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class Hasher:
+    @staticmethod
+    def get_password_hash(plain_password: str) -> str:
+        return pwd_context.hash(plain_password)
+
+    @staticmethod
+    def verify_password(plain_password: str, hashed_password: str) -> bool:
+        return pwd_context.verify(plain_password, hashed_password)
 
 
 class UserModel(Base):
@@ -20,3 +33,6 @@ class UserModel(Base):
     updated_at = Column(DateTime, server_default=func.now(), server_onupdate=func.now())
 
     tasks = relationship("TaskModel", back_populates="user", )
+
+    def verify_password(self, plain_password: str) -> bool:
+        return Hasher.verify_password(plain_password, self.password)
